360a16b65dcb5eac68d340522af904fe4aa06dad
Dani carmona serrat <danielcarmona55@gmail.com>
2017-05-06 23:08:28 +0200

    Add twitter enpoint
    The endpoint now returns creation date of the tweet, username, name, favourites, retweets, if user is verified and the profile image

--- src/api/twitter.js
+++ src/api/twitter.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import getTweets from '../helpers/twitter/getTweets'
 
 let twitterRoutes = Router()
 
 twitterRoutes.get('/', (req, res) => {
-  
+  getTweets(['dan_abramov', 'nodejs']).then((tweets)=>{
+    res.send(tweets)
+  })
 })
 
 export default twitterRoutes
