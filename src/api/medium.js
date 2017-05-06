dc7a7a57ad8adf257c8a0d99b25b07f7a1abdbd2
Dani carmona serrat <danielcarmona55@gmail.com>
2017-05-06 17:38:47 +0200

    Add medium capabilities and helpers
    Made a helper for getting any XML from a url, a helper for converting any xml to json and added a helper to get a regex match

---
+++ src/api/medium.js
@@ -0,0 +1,38 @@
+import { Router } from 'express'
+import Promise from 'bluebird'
+import getMatches from '../helpers/regex/get-matches.js'
+import xml2json from '../helpers/medium/xml2json.js'
+import getXML from '../helpers/medium/getXML.js'
+
+let mediumRoutes = Router()
+
+mediumRoutes.get('/', (req, res) => {
+  const url = 'https://medium.com/feed/the-mission'
+  getXML(url)
+    .then((xml) => {
+      xml2json(xml)
+        .then((articles) => {
+          articles.forEach((article) => {
+            const title = article.title
+            const link = article.link
+            const pubDate = article.pubDate
+            const author = article['dc:creator']
+            const content = article['content:encoded']
+            const getImageRegex = /(<img alt="" src="[\:\w\d\.\/\-\*]*" \/>)/g
+            var image = getMatches(content, getImageRegex, 1)[0]
+            res.send(JSON.stringify({
+              title,
+              link,
+              pubDate,
+              author,
+              content,
+              image
+            }))
+          })
+        })
+        .catch((err)=>console.log("Can't set headers after they are sent ERROR 1"))
+    })
+    .catch((err)=>console.log(err))
+})
+
+export default mediumRoutes
