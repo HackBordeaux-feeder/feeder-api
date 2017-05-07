b883c47bc71a47cb6ef44b575526a487907c3915
Pep Rodeja Ferrer <pepjo26@gmail.com>
2017-05-07 04:05:28 +0200

    Fix

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(cookieParser())
 
 // CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
   res.header('Access-Control-Allow-Credentials', 'true');
