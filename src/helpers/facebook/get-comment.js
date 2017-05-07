b363a8ebe9e638f98ebbef88575b1364c29c035b
hacalox <hanadonc@gmail.com>
2017-05-07 12:23:04 +0200

    Fix Get-comment

--- src/helpers/facebook/get-comment.js
+++ src/helpers/facebook/get-comment.js
@@ -26,16 +26,39 @@ const commentFacebook = (user, target, index, url) => {
       fs.readFile(`${url}/${user}/${index}.html`, "utf-8", (err, data) => {
         if(err) throw err;
         let regex = /<div class="fbUserContent[\s\S]*?>(<div class[\s\S]*?data-utime=[\s\S]*?)<div><form/g
-        console.log(data);
-        let regex_profile = /<div class="fbUserContent[\s\S]*?>(<div class[\s\S]*?data-utime=[\s\S]*?)<div><form/g
+        // console.log(data);
+        let regex_profile = /class="profileLink" href="(.*?)"/
+        let regex_text = /userContent[\s\S]*?<p>(.*?)<\/p>/
+        let regex_name = /https:\/\/scontent[\s\S]*?alt="" aria-label="(.*?)"/
+        let regex_image = /(https:\/\/scontent-.*?)"/
+        let regex_date = /data-utime="(.*?)"/
+        let text = []
+        let time = []
         const articles = data.match(regex)
+
+        articles.map((art) => {
+          let b = art.match(regex_text)
+          text.push(b[1])
+        })
+
+        articles.map((a) => {
+          let b = a.match(regex_date)
+          time.push(b[1])
+        })
+        // let text_final = text.map((tex) => {
+        //   return tex.split('amp;').join('')
+        // })
+        const name = articles[0].match(regex_name)
+        let image = articles[0].match(regex_image)
+        let image_final = image[1].split('amp;').join('')
+        let response = {name: name[1], image:image_final, text:text, time:time }
+        // console.log(response);
         // getMatces(data, regex, articles.length )
-        console.log(articles);
-        resolve(articles)
-        console.log('longitud:',articles.length);
+        // console.log(articles);
+        resolve(response)
+        // console.log('longitud:',articles.length);
       })
     })
   })
 }
-
 export default commentFacebook
